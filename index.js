5f6486df439230344080bf42dc06fd4c7ac3d2a9
Matthew Conlen <mc@mathisonian.com>
2018-11-21 20:16:32 -0800

    bugfixes

--- index.js
+++ index.js
@@ -16,11 +16,20 @@ module.exports = function() {
       _updaters.push(cb);
     }
   }
-
   return function(context) {
     var virtualContext = Object.assign({}, context, contextOverrides);
     args.forEach(function(arg) {
       arg(virtualContext);
+    })
+
+    context.onInitialize && context.onInitialize(function() {
+      _initializers.forEach(_initialize => _initialize.apply(this, arguments));
+    })
+    context.onMount && context.onMount(function() {
+      _mounters.forEach(_mount => _mount.apply(this, arguments));
+    })
+    context.onUpdate && context.onUpdate(function() {
+      _updaters.forEach(_update => _update.apply(this, arguments));
     })
   }
 }
